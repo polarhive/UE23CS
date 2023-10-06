b67a3dfca86e4c4eb57932dcdf68d1be58e169e2
Nathan Paul <mail@polarhive.net>
2023-10-06 08:10:14 +0530

    class: modules

---
+++ random.py
@@ -0,0 +1,9 @@
+import random
+print(random.random())
+print(random.randint(1,100))
+print(random.uniform(2,100))
+print(random.randrange(1,100,2))
+print(random.choice('Hello'))
+
+lst=[1,9]
+print(random.shuffle(lst))
